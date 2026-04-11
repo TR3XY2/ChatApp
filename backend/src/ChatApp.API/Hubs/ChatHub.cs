623db921d6fdbcd684a141f7f5a16c3fd6a5d77e
Yuriy Fundak <yfundak@gmail.com>
2026-04-11 18:57:59 +0300

    Add project files.

---
+++ backend/src/ChatApp.API/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.API.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await this.Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
---
+++ backend/src/ChatApp.Domain/Entities/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Domain.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
